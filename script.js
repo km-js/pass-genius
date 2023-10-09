0cda8e0f314ad6508cbcda5a7c41f52b347ecb70
Kopal Mehta <kopal.mehta22@gmail.com>
2023-10-09 18:16:50 +0530

    uploaded project

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Hello world")
+const length = 12;
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '@#$%^&*()_=-~|[]}][/<>;'
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+const passwordBox = document.getElementById("password");
+const copyImage = document.getElementById("copyImage");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    createPassword();
+})
+
+copyImage.addEventListener("click", function(){
+    copyPassword();
+})
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random()* upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password; 
+}
+
+function copyPassword(){
+navigator.clipboard.writeText(passwordBox.value);
+}
+
+
+
